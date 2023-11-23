09ee4e3cf1c92368929d300175b189f07b89554e
Happistarz <mathieu.raveleau@gmail.com>
2023-11-23 21:09:52 +0100

    base

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Auth: Happistarz
+// Date: 2023/11/20
+// Desc: Main typescript file
+
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// $(function () {
+
+//     setTimeout(function () {
+//         gsap.to(".title span", { opacity: 1, y: -20, duration: 0.5, stagger: 0.05, delay: 0.5 });
+//     }, 200);
+
+// });
