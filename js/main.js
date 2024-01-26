ab5379b4364a83eb079be9ffc944c974fe60fca5
Happistarz <mathieu.raveleau@gmail.com>
2024-01-26 09:56:58 +0100

    project

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@
 // Date: 2023/11/20
 // Desc: Main typescript file
 
+let selectedProject = "all";
 
 gsap.registerPlugin(ScrollTrigger);
 
 $(document).ready(function () {
 
-    gsap.from("#home .home-content", {duration: 1, opacity: 0, y: -75, ease: "power2.out"});
+    gsap.from("#home .home-content", { duration: 1, opacity: 0, y: -75, ease: "power2.out" });
     const arrow = $("#home .home-content a");
 
-    const hoverTL = gsap.timeline({paused: true});
-    hoverTL.to(arrow, {duration: 0.2, y: 10, ease: "power3.out"});
+    const hoverTL = gsap.timeline({ paused: true });
+    hoverTL.to(arrow, { duration: 0.2, y: 10, ease: "power3.out" });
 
     arrow.hover(function () {
         hoverTL.play();
@@ -66,8 +67,18 @@ $(document).ready(function () {
             end: "bottom 80%",
             toggleActions: "restart none none reverse"
         }
-    })
-})
+    });
+
+    function changeProject() {
+        $('#projects .project-content .project').each(function () {
+            if (selectedProject === 'all' || $(this).data('type') === selectedProject) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+});
 
 $('.bottom-navbar ul li a').click(function () {
     $('.bottom-navbar ul li a').removeClass("current");
@@ -77,4 +88,6 @@ $('.bottom-navbar ul li a').click(function () {
 $('#projects .project-content .head button').click(function () {
     $('#projects .project-content .head button').removeClass("selected");
     $(this).addClass("selected");
+    selectedProject = $(this).data('type');
+    changeProject();
 });
